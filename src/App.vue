a787b35be581097ddfb3f85be19e63eb8b5d3595
amira <amira.world@hotmail.fr>
2020-10-13 14:39:57 +0200

    add project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+body {
+  background-image: url("./assets/smoothie-bg.jpg");
+  background-repeat: no-repeat;
+  background-position: top;
+  background-attachment: fixed;
+  background-size: 100%;
+}
+</style>
