feb16343c4024923de5b5df5f5105ae528c9901a
serendipity-coding <56723353+serendipity-coding@users.noreply.github.com>
2020-10-13 14:42:16 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,18 +1,1 @@
-import firebase from 'firebase'
-import firestore from 'firebase/firestore'
-var firebaseConfig = {
-    apiKey: "AIzaSyAUQUM-Hdl6Q3tg6ao1sNln5dsuva1W4hM",
-    authDomain: "smothie-db.firebaseapp.com",
-    databaseURL: "https://smothie-db.firebaseio.com",
-    projectId: "smothie-db",
-    storageBucket: "smothie-db.appspot.com",
-    messagingSenderId: "243726886990",
-    appId: "1:243726886990:web:b9722c1b562b99e3d57b1f",
-    measurementId: "G-YX11SDMJKY"
-};
-// Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-// firebaseApp.firestore().settings({ timestampsInSnapshots: true })
 
-//export firestore database
-export default firebaseApp.firestore()
